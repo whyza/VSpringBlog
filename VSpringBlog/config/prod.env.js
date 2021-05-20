1fecf5868433bb4b3e4f417db0dc87178f583c66
whyza <892903912@qq.com>
2021-05-20 16:06:45 +0800

    comment

--- VSpringBlog/config/prod.env.js
+++ VSpringBlog/config/prod.env.js
@@ -1,8 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"https://iazuresky.com:8443/"',
-  BASE_API: '"http://localhost:8081/"',
+  BASE_API: '"https://iazuresky.com:8443/"',
+  // BASE_API: '"http://localhost:8081/"',
   DEV_API: '"https://iazuresky.com:8443/"'
 
 
